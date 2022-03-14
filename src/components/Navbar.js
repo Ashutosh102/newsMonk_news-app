b4efa7f0632582fec3eefd0004bb755bebcbb0b1
0PiXel0 <ashutosh.punia@yandex.com>
2022-03-14 20:01:00 +0530

    made a new resposive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,16 @@ import React, { Component } from "react";
 export class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary ">
-        <a className="navbar-brand mx-auto" href="#">
-          NewsMonk
-        </a>
+      <nav className='news-nav'>
+          <div className="logo">Newsmonk</div>
+          <div className="rest">
+            <ul>
+              <li><a href="">Home</a></li>
+              
+              <li><a href="">About</a></li>
+            </ul>
+          </div>
 
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active ">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only"></span>
-            </a>
-          </li>
-          <li className="nav-item ">
-            <a className="nav-link" href="#">
-              About
-            </a>
-          </li>
-        </ul>
       </nav>
     );
   }
