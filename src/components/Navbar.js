b2d4b8eae1b964209c33477a0b7a6473081200fa
0PiXel0 <ashutosh.punia@yandex.com>
2022-03-18 03:36:52 +0530

    color scheme adjustment in navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import{FaHome,FaInfo} from "react-icons/fa";
 
 export class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
+      <nav className="navbar navbar-expand-lg navbar-light bg-light ">
+        <a className="navbar-brand " href="#">
+         <span className="logo h1">Newsmonk</span> 
         </a>
         <button
           className="navbar-toggler"
@@ -20,48 +21,50 @@ export class Navbar extends Component {
         </button>
 
         <div className="collapse navbar-collapse " id="navbarSupportedContent">
-          <ul className="navbar-nav my-auto">
-            <li className="nav-item active mx-3">
+          <ul className="navbar-nav sp-font h5">
+            <li className="nav-item active mx-2">
+       
+            <a className="nav-link " href="#">
+                <FaHome/>
+              </a>
+             
+            </li>
+            <li className="nav-item mx-2">
               <a className="nav-link" href="#">
-                Home
+                <FaInfo/>
               </a>
             </li>
-            <li className="nav-item mx-3">
-              <a className="nav-link" href="#">
-                About
-              </a>
-            </li>
-            <li className="nav-item mx-3">
+            <li className="nav-item mx-2">
               <a href="#" className="nav-link">
                 General
               </a>
             </li>
-            <li className="nav-item mx-3">
+            <li className="nav-item mx-2">
               <a href="#" className="nav-link">
                 Business
               </a>
             </li>
-            <li className="nav-item mx-3">
+            <li className="nav-item mx-2">
               <a href="#" className="nav-link">
                 Entertainment
               </a>
             </li>
-            <li className="nav-item mx-3">
+            <li className="nav-item mx-2">
               <a href="#" className="nav-link">
                 Sports
               </a>
             </li>
-            <li className="nav-item mx-3">
+            <li className="nav-item mx-2">
               <a href="#" className="nav-link">
                 Health
               </a>
             </li>
-            <li className="nav-item mx-3">
+            <li className="nav-item mx-2">
               <a href="#" className="nav-link">
                 Science
               </a>
             </li>
-            <li className="nav-item mx-3">
+            <li className="nav-item mx-2">
               <a href="#" className="nav-link">
                 Tech
               </a>
